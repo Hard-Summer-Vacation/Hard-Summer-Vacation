3c7da3bf72a89c68f2b7c478252598405738786e
ishizukahiroya <i.hiroya1215@gmail.com>
2021-11-26 23:27:00 +0900

    eslint

--- src/firebase.js
+++ src/firebase.js
@@ -1,13 +1,13 @@
-import firebase from "firebase";
-import "firebase/firestore";
+import firebase from "firebase"
+import "firebase/firestore"
 
 const firebaseConfig = {
-    apiKey: "AIzaSyDSmAElFjbe7uKaF01AhB-zx36fh8k-2t8",
-    authDomain: "hard-summer-vacation.firebaseapp.com",
-    projectId: "hard-summer-vacation",
-    storageBucket: "hard-summer-vacation.appspot.com",
-    messagingSenderId: "201224479909",
-    appId: "1:201224479909:web:c53baa9d7b6d406942194c"
-  };
+  apiKey: "AIzaSyDSmAElFjbe7uKaF01AhB-zx36fh8k-2t8",
+  authDomain: "hard-summer-vacation.firebaseapp.com",
+  projectId: "hard-summer-vacation",
+  storageBucket: "hard-summer-vacation.appspot.com",
+  messagingSenderId: "201224479909",
+  appId: "1:201224479909:web:c53baa9d7b6d406942194c",
+}
 
-firebase.initializeApp(firebaseConfig);
+firebase.initializeApp(firebaseConfig)
