c48e700f198ee7a546afa673ed454ad24d0e4508
ShotaHonda <tl201226@cis.fukuoka-u.ac.jp>
2021-12-03 23:15:07 +0900

    ホーム画面の設計

--- src/App.vue
+++ src/App.vue
@@ -1,17 +1,16 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png" />
-    <HelloWorld msg="Welcome to Your Vue.js App" />
+    <check />
   </div>
 </template>
 
 <script>
-import HelloWorld from "./components/HelloWorld.vue"
+import check from "./components/check.vue"
 
 export default {
   name: "App",
   components: {
-    HelloWorld,
+    check,
   },
 }
 </script>
---
+++ src/components/check.vue
@@ -0,0 +1,19 @@
+<template>
+  <div>
+    <h1>サブスク</h1>
+    <button v-on:click="clickstart">診断開始</button>
+  </div>
+</template>
+
+<script>
+export default {
+  data: function () {
+    return {}
+  },
+  methods: {
+    clickstart() {},
+  },
+}
+</script>
+
+<style></style>
